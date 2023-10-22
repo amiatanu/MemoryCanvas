55704fa48f9d893b5a727599d11b0ef7ed5a411a
amiatanu <80941558+amiatanu@users.noreply.github.com>
2023-10-22 15:20:50 +0530

    build

---
+++ client/build/precache-manifest.642ce79bc6c5e8784b335aa37ee67ff4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db65cb205a9b4dc18da3d7a911a6174b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a4e1c81bb62030ea3fc",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4e1c81bb62030ea3fc",
+    "url": "/static/js/main.0896d2ce.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
